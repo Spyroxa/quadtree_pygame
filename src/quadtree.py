bb1c9a7f7b4606c7e111b607f78bcca0ba99c52c
Spyroxa <osulebihanaxel@gmail.com>
2023-11-11 02:43:59 +0100

    prefinish projet

--- src/quadtree.py
+++ src/quadtree.py
@@ -1,57 +1,94 @@
 from __future__ import annotations
+import tkinter as tk
 class QuadTree:
     NB_NODES : int = 4
     def __init__(self, hg: bool | QuadTree, hd: bool | QuadTree, bd: bool | QuadTree,bg: bool | QuadTree):
-        self.__hg = hg
-        self.__hd = hd
-        self.__bd = bd
-        self.__bg = bg
+
+        self.hg = hg
+        self.hd = hd
+        self.bd = bd
+        self.bg = bg
 
 
     @property
     def depth(self) -> int:
         """ Recursion depth of the quadtree"""
-        if isinstance(self.hg, QuadTree):
-            return 1 + self.hg.depth  # Vous pouvez prendre n'importe quel fils, tous auront la même profondeur
-        else:
+        if not any(isinstance(child, QuadTree) for child in [self.hg, self.hd, self.bd, self.bg]):
             return 1
-
+        else:
+            depths = [child.depth if isinstance(child, QuadTree) else 1 for child in
+                      [self.hg, self.hd, self.bd, self.bg]]
+            return max(depths) + 1
 
 
     @staticmethod
     def fromFile(filename: str) -> QuadTree:
         """ Open a given file, containing a textual representation of a list"""
         with open(filename, 'r') as file:
-            content = file.read().strip()  # Assuming the file contains a formatted representation of the QuadTree
+            content = file.read().strip()  # En supposant que le fichier contient une représentation formatée du QuadTree
         return QuadTree._fromString(content)
 
+    @staticmethod
+    def _fromString(content: str) -> QuadTree:
+        """Méthode pour créer un QuadTree à partir d'une représentation textuelle"""
 
+        def parse_node(node_data, depth=0):
+            if isinstance(node_data, list):
+                return QuadTree(*[parse_node(child, depth + 1) for child in node_data])
+            else:
+                return int(node_data)
+
+        nodes = parse_node(eval(content))
+        return nodes
 
     @staticmethod
     def fromList(data: list) -> QuadTree:
         """ Generates a Quadtree from a list representation"""
-        pass
+        return QuadTree._fromString(str(data))
 
-class TkQuadTree(QuadTree):
-    def paint(self):
-        """ TK representation of a Quadtree"""
-        if isinstance(self.hg, QuadTree):
-            return f"Q[{self.hg.paint()},{self.hd.paint()},{self.bg.paint()},{self.bd.paint()}]"
-        else:
-            return "B" if self.hg else "W"
 
 @property
 def hg(self):
-        return self.__hg
+        return self.hg
 
 @property
 def hd(self):
-        return self.__hd
+        return self.hd
 
 @property
 def bd(self):
-        return self.__bd
+        return self.bd
 
 @property
 def bg(self):
-        return self.__bg
+        return self.bg
+class TkQuadTree(QuadTree):
+
+    def __init__(self, master=None, **kwargs):
+        super().__init__(**kwargs)
+        self.master = master
+        self.canvas = tk.Canvas(master, width=400, height=400)
+        self.canvas.pack()
+        self.paint()
+
+    def paint(self):
+        """Représentation TK d'un Quadtree"""
+
+        self.canvas.delete("all")  # Effacer le canevas
+        self._paint_quadtree(self, 0, 0, 400, 400)
+
+
+    def _paint_quadtree(self, node, x, y, width, height):
+        """Dessine récursivement le Quadtree sur le canevas"""
+        if isinstance(node, QuadTree):
+            mid_x = x + width / 2
+            mid_y = y + height / 2
+
+            self._paint_quadtree(node.hg, x, y, mid_x - x, mid_y - y)
+            self._paint_quadtree(node.hd, mid_x, y, width - (mid_x - x), mid_y - y)
+            self._paint_quadtree(node.bd, mid_x, mid_y, width - (mid_x - x), height - (mid_y - y))
+            self._paint_quadtree(node.bg, x, mid_y, mid_x - x, height - (mid_y - y))
+        else:
+            color = "black" if node else "green"
+            self.canvas.create_rectangle(x, y, x + width, y + height, fill=color)
+
--- tests/test_quadtree.py
+++ tests/test_quadtree.py
@@ -1,9 +1,14 @@
 
 import sys, os
 sys.path.append(os.path.realpath(os.path.dirname(__file__)+"/.."))
-
+import pytest
 from src import QuadTree, TkQuadTree
-
+import tkinter as tk
+@pytest.fixture
+def tk_root():
+    root = tk.Tk()
+    yield root
+    #root.destroy()
 def test_sample():
     filename = "files/quadtree.txt"
     q = QuadTree.fromFile(filename)
@@ -14,3 +19,64 @@ def test_single():
     q = QuadTree.fromFile(filename)
     assert q.depth == 1
 
+
+def test_create():
+    # Test creating a QuadTree from a list of data
+    data = [
+        [0, 0, 0, [0, 1, 0, 0]],
+        [0, 0, [1, 0, 0, 0], 0],
+        [0, 0, 0, [0, 1, 0, 0]],
+        [0, 0, [0, 1, 0, 0], 0]
+    ]
+    quadtree = QuadTree.fromList(data)
+    assert quadtree.depth == 3
+
+def test_visualization(tk_root):
+    quad_tree = TkQuadTree(master=tk_root, hg=True, hd=False, bd=False, bg=True)
+    tk_root.update_idletasks()
+
+    # Création de l'image
+    tk_root.after(500, lambda: tk_root.destroy())
+    tk_root.mainloop()
+
+def test_quadtree_easy(tk_root):
+    filename = "files/quadtree_easy.txt"
+    q = QuadTree.fromFile(filename)
+    quad_tree = TkQuadTree(master=tk_root, hg=q.hg, hd=q.hd, bd=q.bd, bg=q.bg)
+    tk_root.update_idletasks()
+    tk_root.after(500, lambda: tk_root.destroy())
+    tk_root.mainloop()
+
+def test_quadtree(tk_root):
+    filename = "files/quadtree.txt"
+    q = QuadTree.fromFile(filename)
+    quad_tree = TkQuadTree(master=tk_root, hg=q.hg, hd=q.hd, bd=q.bd, bg=q.bg)
+    tk_root.update_idletasks()
+    tk_root.after(500, lambda: tk_root.destroy())
+    tk_root.mainloop()
+
+def test_create_from_data(tk_root):
+    data = [
+        [0, 0, 0, [0, 1, 0, 0]],
+        [0, 0, [1, 0, 0, 0], 0],
+        [0, 0, 0, [0, 1, 0, 0]],
+        [0, 0, [0, 1, 0, 0], 0]
+    ]
+    q = QuadTree.fromList(data)
+    quad_tree = TkQuadTree(master=tk_root, hg=q.hg, hd=q.hd, bd=q.bd, bg=q.bg)
+    tk_root.update_idletasks()
+    tk_root.after(500, lambda: tk_root.destroy())
+    tk_root.mainloop()
+
+def test_create_creeper(tk_root):
+    data = [
+        [0,0,[1,0,0,1],[0,1,1,0]],
+        [0,0,[1,0,0,1],[0,1,1,0]],
+        [[1,0,1,1],0,0,[1,1,1,0]],
+        [0,[0,1,1,1],[1,1,0,1],0]
+    ]
+
+    q = QuadTree.fromList(data)
+    quad_tree = TkQuadTree(master=tk_root, hg=q.hg, hd=q.hd, bd=q.bd, bg=q.bg)
+    tk_root.update_idletasks()
+    tk_root.mainloop()
